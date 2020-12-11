eee84668afa3bbdedfecd24ffd656b6a3ce399d4
AlexanderT <38465150+AlexaTack@users.noreply.github.com>
2020-12-12 00:20:02 +0100

    Added static files

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const app = express();
 const fetch = require('node-fetch');
 const cors = require('cors');
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log(`Listening on ${port}.`));
 
 app.use(cors({
     origin: '*',
     methods: ['GET']
 }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/facts', async (req, res) => {
     const wiki_url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=angular';
