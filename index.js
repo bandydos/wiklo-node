f01ff81ee13578030f3d2e190d5ed913e5fd3736
AlexanderT <38465150+AlexaTack@users.noreply.github.com>
2020-12-18 17:26:18 +0100

    Added basic layout

--- index.js
+++ index.js
@@ -13,12 +13,21 @@ app.use(cors({
 }));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/facts', async (req, res) => {
+app.get('/facts', (req, res) => {
+    res.send('helo')
+})
+
+app.get('/api/facts', async (req, res) => {
     const wiki_url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=angular';
     const response = await fetch(wiki_url);
-    if (!response.ok) console.log(`Something went wrong, status: ${response.status}.`);
+    if (!response.ok) {
+        console.log(`Something went wrong, status: ${response.status}.`);
+        return;
+    }
     res.json(await response.json());
 });
